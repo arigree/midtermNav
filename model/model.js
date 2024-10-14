43575300708f231d942fac806de7341a804eac50
evanjschmitt <evanjschmitt@gmail.com>
2024-10-14 09:59:22 -0400

    Routing Functions Started * Added routing to App and Model.js * Created subpages for site

---
+++ model/model.js
@@ -0,0 +1,18 @@
+export function changePage(pageName) {
+    console.log("pageName:", pageName);
+    if (pageName == "") {
+      // $("#app").html(`<h1>home</h1>`);
+      $.get("pages/home.html", (data) => {
+        $("#app").html(data);
+      }).fail((error) => {
+        console.log("error", error);
+    });
+} else {
+    // $("#app").html(`<h1>${pageName}</h1>`);
+    $.get(`pages/${pageName}.html`, (data) => {
+      $("#app").html(data);
+    }).fail((error) => {
+      console.log("error", error);
+    });
+  }
+}
